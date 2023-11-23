77eb6adbe245b178e4dc3fb81adbe1362bb710f8
michaelazvo <145450127+michaelazvo@users.noreply.github.com>
2023-11-23 15:57:12 +0100

    add Main Scene

---
+++ src/main/java/sk/comma/MainScene.java
@@ -0,0 +1,27 @@
+package sk.comma;
+
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainScene extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //vzinkne objekt tejto triedy
+        MainSceneController controller = new MainSceneController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainSceneComma.fxml"));
+
+        loader.setController(controller);
+        Parent parent = loader.load();
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("comma");
+        primaryStage.show();
+
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
---
+++ src/main/java/sk/comma/MainSceneController.java
@@ -0,0 +1,19 @@
+package sk.comma;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class MainSceneController {
+    @FXML
+    private Button prihlaseniePorotcaButton;
+
+    @FXML
+    private Button prihlasitTanecneTelesoButton;
+
+    @FXML
+    private ComboBox<?> sutazCombobox;
+
+    @FXML
+    private Button zobrazitVysledkyButton;
+}
