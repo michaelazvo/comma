fe9368f2e633d568cb4834bb785c45bbb5f1e094
syraee <andrejkovaalzbeta@gmail.com>
2023-12-20 22:27:03 +0100

    hashovanie

---
+++ src/main/java/sk/comma/Hashovanie.java
@@ -0,0 +1,11 @@
+package sk.comma;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class Hashovanie {
+
+    public static String hashovanie(String heslo, String sol) {
+        String pwHash = BCrypt.hashpw(heslo, sol);
+        return pwHash;
+    }
+}
